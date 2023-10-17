765704f64e529b76584fd12812607a6bb1d67359
Joris Benmehal <benmehal.joris@gmail.com>
2023-10-17 20:41:58 +0200

    :sparkles: Add click functionality to gallery images

--- assets/gallery.js
+++ assets/gallery.js
@@ -1,15 +1,27 @@
+let thumbnails;
+let filteredThumbnails = [];
+
 document.addEventListener("DOMContentLoaded", function () {
-  let lists = document.querySelectorAll(".list");
-  lists.forEach(function (list) {
+  thumbnails = document.querySelectorAll(".thumbnail");
+  filteredThumbnails = Array.from(thumbnails);
+
+  document.querySelectorAll(".list").forEach((list) => {
     list.addEventListener("click", function () {
+      document.querySelectorAll(".list").forEach((sibling) => {
+        sibling.classList.remove("active");
+      });
+      list.classList.add("active");
+
       const dataValue = list.getAttribute("data-filter");
       let items = document.querySelectorAll(".gallery-item");
       if (dataValue === "all-items") {
-        items.forEach(function (item) {
-          item.style.display = "block";
-        });
+        filteredThumbnails = Array.from(thumbnails);
+        items.forEach((item) => (item.style.display = "block"));
       } else {
-        items.forEach(function (item) {
+        filteredThumbnails = Array.from(items).filter((item) =>
+          item.classList.contains(dataValue)
+        );
+        items.forEach((item) => {
           if (!item.classList.contains(dataValue)) {
             item.style.display = "none";
           } else {
@@ -19,13 +31,56 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
-});
 
-document.querySelectorAll(".list").forEach((list) => {
-  list.addEventListener("click", function () {
-    document.querySelectorAll(".list").forEach((sibling) => {
-      sibling.classList.remove("active");
+  const fullscreenContainer = document.getElementById("fullscreen-background");
+  const fullscreenImage = document.getElementById("fullscreen-image");
+  const prevButton = document.getElementById("prev-button");
+  const nextButton = document.getElementById("next-button");
+  let currentIndex = 0;
+
+  filteredThumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener("click", () => {
+      const imgSrc = thumbnail.src;
+
+      fullscreenImage.src = imgSrc;
+      fullscreenContainer.style.display = "block";
+      currentIndex = index;
     });
-    list.classList.add("active");
+  });
+
+  prevButton.addEventListener("click", () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+    } else {
+      currentIndex = filteredThumbnails.length - 1;
+    }
+    const currentElement = filteredThumbnails[currentIndex];
+    const imgElement = currentElement.querySelector("img");
+    if (imgElement) {
+      fullscreenImage.src = imgElement.src;
+    } else {
+      fullscreenImage.src = filteredThumbnails[currentIndex].src;
+    }
+  });
+
+  nextButton.addEventListener("click", () => {
+    if (currentIndex < filteredThumbnails.length - 1) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+    const currentElement = filteredThumbnails[currentIndex];
+    const imgElement = currentElement.querySelector("img");
+    if (imgElement) {
+      fullscreenImage.src = imgElement.src;
+    } else {
+      fullscreenImage.src = filteredThumbnails[currentIndex].src;
+    }
+  });
+
+  fullscreenContainer.addEventListener("click", (e) => {
+    if (e.target.id === "fullscreen-background") {
+      fullscreenContainer.style.display = "none";
+    }
   });
 });
